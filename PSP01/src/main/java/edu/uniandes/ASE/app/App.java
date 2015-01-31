6ae9486ade83c47aad4564b4b539435b9908a098
Diego Lozano <diego41dc@hotmail.com>
2015-01-31 02:32:54 -0500

    a

--- PSP01/src/main/java/edu/uniandes/ASE/app/App.java
+++ PSP01/src/main/java/edu/uniandes/ASE/app/App.java
@@ -1,5 +1,12 @@
 package edu.uniandes.ASE.app;
 
+import java.awt.BorderLayout;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,35 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        File f = new File(System.getProperty("user.dir"));
+        App.dir(f);
+        
+    }
+    public static void dir(File f) {
+        if (f.exists()) {
+            if (f.isDirectory()) {
+                //System.out.println(f.getAbsolutePath());
+                if(f.listFiles() != null){
+                    for (File archivo : f.listFiles()) {
+                       dir(archivo);
+                    }
+                }
+            } else if (f.isFile() && f.getName().endsWith(".java")) {
+                 System.out.println(f.getAbsolutePath());
+                 leer(f);
+            }
+        }
+    }
+    public static void leer(File f){
+        try {
+            BufferedReader f2 = new BufferedReader(new FileReader(f));
+            String linea;
+            while((linea = f2.readLine()) != null){
+                System.out.println(linea); 
+           }
+        } catch (IOException e) {
+         e.printStackTrace();
+       } 
+   
     }
 }
