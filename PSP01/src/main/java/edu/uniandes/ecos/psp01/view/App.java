c439140911b804bea0b365bec145657c581f3eec
diego.lozano <diego.lozano@DT1021.datatools.local>
2015-02-02 18:17:30 -0500

    AJustes

---
+++ PSP01/src/main/java/edu/uniandes/ecos/psp01/view/App.java
@@ -0,0 +1,59 @@
+package edu.uniandes.ecos.psp01.view;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static Integer lineas = 0;
+    public static void main( String[] args )
+    {
+        File f = new File(System.getProperty("user.dir"));
+        App.dir(f);
+        
+    }
+    public static void dir(File f) {
+        if (f.exists()) {
+            if (f.isDirectory()) {
+                //System.out.println(f.getAbsolutePath());
+                if(f.listFiles() != null){
+                    for (File archivo : f.listFiles()) {
+                       dir(archivo);
+                    }
+                }
+            } else if (f.isFile() && f.getName().endsWith(".java")) {
+                 System.out.println("Path:"+f.getAbsolutePath());
+                 leer(f);
+            }
+        }
+    }
+    public static void leer(File f){
+        try {
+            BufferedReader f2 = new BufferedReader(new FileReader(f));
+            String linea;
+            while((linea = f2.readLine()) != null){
+               // System.out.println(linea); 
+                contarLineas(linea);
+           }
+            System.out.println("Lineas:"+lineas);
+        } catch (IOException e) {
+         e.printStackTrace();
+       } 
+   
+    }
+    public static void contarLineas(String linea){
+        if(linea != null && linea.trim() != ""){
+            lineas++;
+            
+        }
+    }
+    public static void contarMetodos(){
+        
+    }
+}
